eef6b1777fb77492f66983aca4d16c43b2de3923
lydiali9 <648823000@qq.com>
2019-11-03 13:29:05 +0800

    [feature]: happypack多线程打包

---
+++ history/webpack.config-dllPlugin.js
@@ -0,0 +1,45 @@
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        contentBase: './dist'
+    },
+    module: {
+        noParse: /jquery/, // 不去解析jquery中的依赖库
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: path.resolve('src'),
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DllReferencePlugin({
+            manifest: path.resolve(__dirname, 'dist', 'mainfest.json')
+        }),
+        new webpack.IgnorePlugin(/\.\/local/, /moment/), // 从moment库中忽略.local文件
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ]
+}
