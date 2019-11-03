763adef4db12ff1524fe42c631d7acc3974b5892
lydiali9 <648823000@qq.com>
2019-11-03 10:20:59 +0800

    [feature]: module下noParse不去解析配置的第三方依赖库 加速打包文件的速度

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        noParse: /jquery/, // 不去解析jquery中的依赖库
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+
+        })
+    ]
+}
